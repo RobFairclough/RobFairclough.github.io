85d2559519dbc31c745e9dc077d78e35bac4a908
Rob Fairclough <rob.a.fairclough@gmail.com>
2018-06-06 18:46:44 +0100

    add scroll animations not yet functional and update bio

--- js/index.js
+++ js/index.js
@@ -1,5 +1,13 @@
-$("#home-button").click(function() {
-  $(html, body).animate({
-    scrolltop: $home-link.offset().top
-  }, 2000);
+$(document).ready(function () {
+    $( ".nav-link" ).click(function ( event ) {
+        event.preventDefault();
+        $(html, body).animate({ scrollTop: $($(this).attr("href")).offset().top }, 2000);
+    });
+});
+
+/*$(document).ready(function(){
+    $( "a.scrollLink" ).click(function( event ) {
+        event.preventDefault();
+        $("html, body").animate({ scrollTop: $($(this).attr("href")).offset().top }, 500);
+    });
 });
