019ca9f8eb853bd3e20c19414965d0db06a7b982
Rob Fairclough <rob.a.fairclough@gmail.com>
2018-06-08 19:24:30 +0100

    comment on js function and update image for weather app

--- js/index.js
+++ js/index.js
@@ -1,6 +1,6 @@
 $(document).ready(function () {    
 
-    $( ".nav-link" ).click(function () {
+    $( ".nav-link" ).click(function () { //when a .nav-link is clicked, scroll to the href of the link
         var linkTo = $(this).attr('href');
         $('html, body').animate({ scrollTop: $(linkTo).offset().top }, 2000);
     });
